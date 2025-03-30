b31d549aebab8855752bb527ec5924939b5082e8
Esvinded <phong.vnk228033@sis.hust.edu.vn>
2025-03-30 13:27:58 +0700

    Initial project structure setup

---
+++ src/test/java/com/_prj/MovieTheatresManagement/MovieTheatresManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com._prj.MovieTheatresManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTheatresManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
